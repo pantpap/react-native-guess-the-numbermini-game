d1720028a89bcd1ec6e12fba47e5d57e23319d57
PAPASAVVAS Pantelis <dg17admin@IIL2316.home>
2023-08-04 13:25:41 +0300

    add shadows for android and iOS

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -13,10 +13,17 @@ const StartGameScreen = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     marginTop: 100,
+    marginHorizontal: 24,
+    borderRadius: 8,
+    elevation: 4, // add shadow to the element. Works only for android
     padding: 16,
-    backgroundColor: '#72063c'
+    backgroundColor: '#72063c',
+    // Adding shadow for IOS devices
+    shadowColor: 'black',
+    shadowOffset: {width: 0, height: 2},
+    shadowRadius: 6,
+    shadowOpacity: 0.25
   }
 })
 
