9818b67609e9d5c9ad4266ff6f1a601053fba82d
PAPASAVVAS Pantelis <dg17admin@IIL2316.home>
2023-08-04 13:15:19 +0300

    add styling on StartGameScreen

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,14 +1,23 @@
-import { TextInput, View } from 'react-native'
+import { TextInput, View, StyleSheet } from 'react-native'
 import PrimaryButton from "../components/PrimaryButton";
 
 const StartGameScreen = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <TextInput/>
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 100,
+    padding: 16,
+    backgroundColor: '#72063c'
+  }
+})
 
 export default StartGameScreen;
