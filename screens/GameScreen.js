4799614994635db7eb1a3ada8c6fbcf02eb8dbd7
Pantelis Papasavvas <pantelis.papasavvas@netcompany-intrasoft.com>
2023-08-16 14:56:58 +0300

    feat(GameScreen): add plus and minus controls

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,7 +1,8 @@
-import {StyleSheet, Text, View} from "react-native";
+import {Alert, StyleSheet, Text, View} from "react-native";
 import Title from "../components/ui/Title";
 import {useState} from "react";
 import NumberContainer from "../components/game/NumberContainer";
+import PrimaryButton from "../components/ui/PrimaryButton";
 
 const generateRandomNumber = (min, max, exclude) => {
   const randomNumber = Math.floor(Math.random() * (max - min)) + min;
@@ -13,15 +14,37 @@ const generateRandomNumber = (min, max, exclude) => {
   }
 }
 
+let min = 1;
+let max = 100
+
 const GameScreen = ({userInput}) => {
-  const initialNumber = generateRandomNumber(1, 100, userInput)
+  const initialNumber = generateRandomNumber(min, max, userInput)
   const [currentGuess, setCurrentGuess] = useState(initialNumber);
+
+  const handlePress = (operator) => {
+    if((operator === 'minus' && currentGuess < userInput) || (operator === 'plus' && currentGuess > userInput)){
+      Alert.alert('You MUST play fair',
+        'You know that you are cheating...',
+        [{text: 'try again', style: 'cancel'}])
+    }
+    if(operator === 'minus'){
+      max = currentGuess
+    } else {
+      min = currentGuess;
+    }
+    setCurrentGuess( generateRandomNumber(min, max, currentGuess))
+  }
+
   return (
     <View style={styles.screen}>
       <Title>Players Choice</Title>
       <NumberContainer>{currentGuess}</NumberContainer>
       <View>
         <Text>Higher or Lower?</Text>
+        <View>
+          <PrimaryButton onPress={() => handlePress('plus')}>+</PrimaryButton>
+          <PrimaryButton onPress={() => handlePress('minus')}>-</PrimaryButton>
+        </View>
       </View>
       <View>
         <Text>Log Rounds</Text>
