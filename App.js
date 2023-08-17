1a5846c73ac96afd57ac95e16aa649dcfb6f38cc
Pantelis Papasavvas <pantelis.papasavvas@netcompany-intrasoft.com>
2023-08-17 14:33:01 +0300

    feat(App): add GameOverScreen

--- App.js
+++ App.js
@@ -4,18 +4,28 @@ import {LinearGradient} from "expo-linear-gradient";
 import {useState} from "react";
 import GameScreen from './screens/GameScreen'
 import Colors from "./constanst/colors";
+import GameOverScreen from "./screens/GameOverScreen";
 
 export default function App() {
   const [userInput, setUserInput] = useState();
+  const [gameIsOver, setGameIsOver] = useState(false);
 
   const userInputHandler = (inputNumber) => {
     setUserInput(inputNumber);
   }
 
+  const gameOverHandler = () => {
+    setGameIsOver(true);
+  }
+
   let screen = <StartGameScreen onConfirmInput={userInputHandler}/>
 
   if(userInput){
-    screen = <GameScreen userInput={userInput} />
+    screen = <GameScreen userInput={userInput} onGameOver={gameOverHandler} />
+  }
+
+  if(gameIsOver){
+    screen = <GameOverScreen/>
   }
   return (
     <LinearGradient colors={[Colors.primary700 ,Colors.primaryYellow]} style={styles.rootScreen}>
