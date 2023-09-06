b5b5bdf0d916a1eb83a0e78dce340129d4a79b82
Pantelis Papasavvas <pantelis.papasavvas@netcompany-intrasoft.com>
2023-09-06 18:49:24 +0300

    feat(GameOver): add and style image in tha GameOverScreen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,9 +1,39 @@
-import {Text} from "react-native";
+import {View, Image, StyleSheet, Text} from "react-native";
+import Title from '../components/ui/Title';
+import Colors from '../constanst/colors';
+
 
 const GameOverScreen = () => {
   return (
-    <Text>Game is Over !</Text>
+   <View style={styles.rootContainer}>
+    <Title>GAME OVER</Title>
+    <View style={styles.imageContainer}>
+      <Image style={styles.image} source={require('../assets/images/success.png')}/>
+    </View>
+   </View>
   )
 }
 
 export default GameOverScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    padding: 24,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: Colors.primary800,
+    overflow: 'hidden',
+    margin: 36
+  },
+  image:{
+    width: '100%',
+    height: '100%'
+  }
+})
